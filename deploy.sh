5b0bdd8c07db4b271c3566375adcb586358e4f76
Stevefankem <fankemsteve@ymail.com>
2024-11-26 12:53:22 -0500

    deploy master update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ Developers are at work
 Application is ready to be deployed to the development environment.
 deplopment must be reviewed by managers before deployment
 SDLC is great
-
+AWS codedeploy is a great tool.
